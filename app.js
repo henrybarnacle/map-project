91b95b6d17623988260578b29d9e8cb8a8ed2182
Henry Barnacle <henrybarnacle@Henrys-MBP.fios-router.home>
2017-04-24 17:16:07 -0400

    added location markers and show/hide listings buttons

--- app.js
+++ app.js
@@ -1,17 +1,73 @@
 
 var map;
-function initMap () {
+var markers = [];
+
+function initMap() {
 	map = new google.maps.Map(document.getElementById('map'),{
 		center: {lat: 40.7313779, lng: -73.9546765},
-		zoom: 15
+		zoom: 13
 	});
-		var home = {lat: 40.7259861, lng: -73.9555063};
+	var locations = [
+	{title: 'park ave penthouse', location: {lat: 40.7713024, lng: -73.9632293}},
+	 {title: 'Chelsea loft', location: {lat: 40.7444883, lng: -73.9949465}},
+	 {title: 'union square loft', location: {lat: 40.7347062, lng: -73.9895759}}
+	 ];
+
+	 var largeInfowindow = new google.maps.InfoWindow();
+	 var bounds = new google.maps.LatLngBounds();
+
+	 for (var i = 0; i < locations.length; i++) {
+
+	 	var position = locations[i].location;
+	 	var title = locations[i].title;
+
 		var marker = new google.maps.Marker({
-			position: home,
-			map: map,
-			title: 'first marker'
+		
+		position: position,
+		title: title,
+		animation: google.maps.Animation.DROP,
+		id: i
 		});
+
+		markers.push(marker);
+		bounds.extend(marker.position);
+		
+		marker.addListener('click', function() {
+			populateInfoWindow(this, largeInfowindow);
+
+		});
+	}
+	
+
+		document.getElementById('show-listings').addEventListener('click', showListings);
+		document.getElementById('hide-listings').addEventListener('click', hideListings);
+
 }
 
+function populateInfoWindow(marker, infowindow) {
+	if (infowindow.marker != marker) {
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.open(map, marker);
+		infowindow.addListener('closeclick', function() {
+			infowindow.setMarker(null);
+			
+		});
+	}
+}
 
+function showListings() {
+	var bounds = new google.maps.LatLngBounds();
+
+	for (i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+		bounds.extend(markers[i].position);
+	}
+	map.fitBounds(bounds);
+}
 	
+	function hideListings() {
+		for (i = 0; i < markers.length; i++) {
+			markers[i].setMap(null);
+		}
+	}
